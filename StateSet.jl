5aa4b40e6cfced09b4a0a53c8647dc26a3be09cf
isaac patterson <isaacthepatterson@gmail.com>
2024-05-06 20:43:25 -0400

    started code

--- StateSet.jl
+++ StateSet.jl
@@ -1,4 +1,11 @@
 using Graphs
+using GraphPlot
+using Compose
+using QuantumClifford
+using Random
 
-g = path_graph(6)
+num_vertices = rand(4:10)
+max_edges = Int(num_vertices*(num_vertices-1)/2)
+num_edges = rand(0:max_edges)
+g = SimpleGraph(num_vertices, num_edges)
 println(g)
